8eaf6b7f64ed5b3ca90a79accf6da22daaccf06a
kodokunax-sudo <kodokunax@gmail.com>
2026-05-12 09:57:16 +0500

    Update images_evolutionary.js

--- images_evolutionary.js
+++ images_evolutionary.js
@@ -1,1 +1,7 @@
-
+const cardImagesEvolutionary = {
+    "Луффи : Король пиратов": "images/luffy_pirate_king.gif",
+    "Сайтама/Гароу": "images/saitama_garou.gif",
+    "Гарп/Кудзан": "images/garp_kuzan.gif",
+    "Семёрка": "images/the_seven.gif",
+    "Уильям Фрэнсис": "images/william_francis.gif",
+};
